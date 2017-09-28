cbb3477b60d99a17566e4e8170712dc0b8ed79d1
Lena Langholf <lena@mehh79.kybi>
2017-09-28 15:02:13 +0300

    deleted old files

--- TestOutput.py
+++
@@ -1,9 +0,0 @@
-import os
-from Tkinter import *
-Outputfileobject=os.popen("./FibreSeperator")
-Output=Outputfileobject.read()
-Outputfileobject.close()
-root=Tk()
-root.title("Output text")
-Text=Label(root,text=Output).pack()
-root.mainloop()
